2114f9c5f906c98dcae8011d2471f31bd364a2f1
Luke Shumard <lukeshumard@gmail.com>
2018-07-05 10:06:30 +0100

    api tests

--- test/api.js
+++ test/api.js
@@ -3,11 +3,18 @@ const chai = require('chai');
 const {expect} = chai;
 const API = require('../lib/api');
 
-describe('ThemeKit API', () => {
+describe('API', () => {
 
-  it('Should call method', () => {
-    const apiCall = API('install');
-    expect(apiCall).to.equal('install');
+  it('Should not allow non-Array', () => {
+    const argStr = () => { API('install'); };
+    const argObj = () => { API({}); };
+    expect(argStr).to.throw('API Commands is not an Array');
+    expect(argObj).to.throw('API Commands is not an Array');
+  });
+
+  it('Should be able to run', () => {
+    const apiCall = API(['test-command']);
+    expect(apiCall).to.not.throw;
   });
 
 });
