5b3754f25fa6fdb5853a1d3739e30f477e8d5375
Luke Shumard <lukeshumard@gmail.com>
2018-07-18 13:31:54 +0100

    update tests + enforce callback as fn

--- test/api.js
+++ test/api.js
@@ -1,20 +1,38 @@
 const chai = require('chai');
+const sinon = require('sinon');
+const sinonChai = require('sinon-chai');
 
 const {expect} = chai;
+chai.use(sinonChai);
 const API = require('../lib/api');
 
 describe('API', () => {
 
-  it('Should not allow non-Array', () => {
+  it('Should only allow Array as command', () => {
     const argStr = () => { API('install'); };
     const argObj = () => { API({}); };
     expect(argStr).to.throw('API Commands is not an Array');
     expect(argObj).to.throw('API Commands is not an Array');
   });
 
-  it('Should be able to run', () => {
-    const apiCall = API(['test-command']);
+  it('Should only allow Function as callback', () => {
+    const argStr = () => { API(['test-command'], ''); };
+    const argObj = () => { API(['test-command'], {}); };
+    const argBlank = () => { API(['test-command']); };
+    expect(argStr).to.throw('API Callback is not a Function');
+    expect(argObj).to.throw('API Callback is not a Function');
+    expect(argBlank).to.not.throw;
+  });
+
+  it('Should execute callback', (done) => {
+    const apiCallback = sinon.spy();
+    const apiCall = API(['test-command'], apiCallback);
     expect(apiCall).to.not.throw;
+    setTimeout(() => {
+      expect(apiCallback).to.have.been.calledOnce;
+      expect(apiCallback).to.have.been.calledWith();
+      done();
+    }, 50);
   });
 
 });
